aec622b76a4106b11b4a8f5e1e068f5f6df5a957
Alumno <Alumno@PC36>
2025-08-27 12:14:14 -0300

    Agregar archivos de proyecto.

---
+++ pryPereiroGitHub/pryGithub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryPereiroGitHub
+{
+    public partial class pryGithub : Form
+    {
+        public pryGithub()
+        {
+            InitializeComponent();
+        }
+
+        private void pryGithub_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
